bbae1403e1b244b289f3569c58bb25c8d5d8267b
SebastienMoeller <sebastienmoeller1@gmail.com>
2018-01-18 16:11:45 +0100

    Implemented star review scrap
    Need to figure out how to get to the next page and possibly avoid
    duplicates. The data is updating so frequently it changes within 5
    minutes from personal observations.

--- GoogleReviewScrap.py
+++ GoogleReviewScrap.py
@@ -32,20 +32,33 @@ for i in scrap:
 
 
 #%%
-# Scrap the star rating
+# Scrap the star rating with noise
 url = 'https://www.google.com/shopping/product/5933462846166885821/reviews?output=search&q=iphone+x&oq=iphone+x&prds=paur:ClkAsKraX_rF40LQy2-BzkgE8wgr55aIFvSNoLYTvzWp6ulZKN4SpoI7JqPpChbztn5oHhXayw0IKumMhPjVOPJAvphyMoDnPXS1BcAGxwJNxOu6YHRwRxy_hxIZAFPVH71vJWF51zJw3MuI7eIHMGOCaLxvHA&sa=X&ved=0ahUKEwia_pa7zuHYAhUDbFAKHeWAAdEQqSQIqwE'
 page = urll.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
 scrap = soup.find_all('div', attrs={'class':'_OBj'})
 
+test = []
 for i in scrap:
-    print('')
-    print(i)
+    test.append(str(i))
+#%% Extract the rating
+rating = []
+for i in test:
+    idx = i.find('aria-label="')+len('aria-label="')
+    rating.append(int(i[idx]))
+
+rating = rating[1:]
+#%%
+
+
+    
 #%%
     
-# Scrap star rating and add to data structure
+# Add star rating to data structure
+idx = 0
 for i in reviews:
-    i.append('rating')
+    i.append(rating[idx])
+    idx = idx + 1
 #%%
 # Retrieve rating
 reviews[0][1]
