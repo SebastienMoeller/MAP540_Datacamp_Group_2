dc90c4b71863f2195bc7a0bb71522c3ccb7b7c8e
SebastienMoeller <sebastienmoeller1@gmail.com>
2018-01-18 15:38:42 +0100

    Succeeded in scrapping star rating
    Need to append to review and then figure out how to access the next
    page of reviews

--- GoogleReviewScrap.py
+++ GoogleReviewScrap.py
@@ -11,11 +11,12 @@ import urllib.request as urll
 from bs4 import BeautifulSoup
 #%%
 url = 'https://www.google.com/shopping/product/5933462846166885821/reviews?output=search&q=iphone+x&oq=iphone+x&prds=paur:ClkAsKraX_rF40LQy2-BzkgE8wgr55aIFvSNoLYTvzWp6ulZKN4SpoI7JqPpChbztn5oHhXayw0IKumMhPjVOPJAvphyMoDnPXS1BcAGxwJNxOu6YHRwRxy_hxIZAFPVH71vJWF51zJw3MuI7eIHMGOCaLxvHA&sa=X&ved=0ahUKEwia_pa7zuHYAhUDbFAKHeWAAdEQqSQIqwE'
-page2 = urll.urlopen(url)
-soup2 = BeautifulSoup(page2, 'html.parser')
+page = urll.urlopen(url)
+soup = BeautifulSoup(page, 'html.parser')
 #%%
 # scrap reviews
-scrap = soup2.find_all('div', attrs={'class':'review-content'})
+scrap = soup.find_all('div', attrs={'class':'review-content'})
+print(scrap)
 #%%
 # Save reviews in a list
 reviews = []
@@ -25,15 +26,49 @@ for i in scrap:
     meta.append(i.text)
     reviews.append(meta)
 #%%
-# Scrap the star rating
-    
+
+
+
+
+
 #%%
+# Scrap the star rating
+url = 'https://www.google.com/shopping/product/5933462846166885821/reviews?output=search&q=iphone+x&oq=iphone+x&prds=paur:ClkAsKraX_rF40LQy2-BzkgE8wgr55aIFvSNoLYTvzWp6ulZKN4SpoI7JqPpChbztn5oHhXayw0IKumMhPjVOPJAvphyMoDnPXS1BcAGxwJNxOu6YHRwRxy_hxIZAFPVH71vJWF51zJw3MuI7eIHMGOCaLxvHA&sa=X&ved=0ahUKEwia_pa7zuHYAhUDbFAKHeWAAdEQqSQIqwE'
+page = urll.urlopen(url)
+soup = BeautifulSoup(page, 'html.parser')
+scrap = soup.find_all('div', attrs={'class':'_OBj'})
+
+for i in scrap:
+    print('')
+    print(i)
+#%%
+    
 # Scrap star rating and add to data structure
 for i in reviews:
     i.append('rating')
 #%%
 # Retrieve rating
 reviews[0][1]
+#%%
+
+#%%
+
+#%%
+
+#%%
+
+#%%
+
+#%%
+
+#%%
+
+#%%
+
+#%%
+
+#%%
+
 #%%
 
 #%%
