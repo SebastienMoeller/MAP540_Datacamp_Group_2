a9032d40b52f087c4b8e3087297cc3c5851dc9e4
SebastienMoeller <sebastienmoeller1@gmail.com>
2018-01-23 12:41:58 +0100

    counting tokens
    need to figure out how to print a subset of the counter output, not
    interested in single occurrence combinations or words

--- Corpus Analysis.py
+++ Corpus Analysis.py
@@ -8,11 +8,13 @@ Created on Fri Jan 19 21:15:31 2018
 
 #%%
 import pandas as pd
-from textblob import TextBlob
+from nltk.corpus import stopwords
 from nltk.tokenize import TweetTokenizer
+import collections
 #%%
 data = pd.read_csv('GoogleiPhoneX.csv', index_col = 0)
 comments = data['comments'].tolist()
+#comments = ['I LOVE this phone, my Wife has one..... @wifey #waddup. I\'ll be going in-to univErsiTY in 4 days. I won\'t give up until I\'ve won this phone phone phone please ~']
 #%%
 
 
@@ -22,10 +24,11 @@ comments = data['comments'].tolist()
 #%%
 l = [item.lower() for item in comments]
 #%% We establish a dictionary of the transformation
-transformation = {a:" " for a in ['@','/','#','.','\\','!']}
+transformation = {a:" " for a in ['@','/','#','.','\\','!',',']}
 text = ' '.join(l)                                  
 text = text.translate(str.maketrans(transformation))
-#print(text)
+
+print(text)
 #%%
 
 
@@ -33,25 +36,27 @@ text = text.translate(str.maketrans(transformation))
 
 
 #%%
-blob = TextBlob(text)
-listOfWords = blob.words
+english_stopwords = set(stopwords.words('english'))
 #%%
-tkzer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+tkzer = TweetTokenizer(preserve_case = False, strip_handles = True, reduce_len = True)
 token = tkzer.tokenize(text)
 #%%
-
+tokens = [i for i in token if i not in english_stopwords]
 #%%
 
 #%%
+def nGrams(input_list, n):
+    return list(zip(*[input_list[i:] for i in range(n)]))
+#%%
+bigram = nGrams(tokens, 2)
+trigram = nGrams(tokens, 3)
 
 #%%
-
+counter=collections.Counter(tokens)
+print(counter)
 #%%
-
-#%%
-
-#%%
-
+countBigram = collections.Counter(bigram)
+countTrigram = collections.Counter(trigram)
 #%%
 
 #%%
