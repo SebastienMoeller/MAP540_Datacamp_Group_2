c829f597fc89c469a5316ff3167e453352923a97
SebastienMoeller <sebastienmoeller1@gmail.com>
2018-02-01 15:21:34 +0100

    New File

---
+++ LDA Analysis.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  1 14:17:00 2018
+
+@author: Sebastien
+"""
+
+#%%
+import pandas as pd
+
+
+#%%
+corpus = pd.read_csv('tfidf_monogram.csv')
+
+#%%
+
+
+
